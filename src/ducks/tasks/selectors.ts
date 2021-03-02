7d1abefe2e1689b0a012bd005fd9274d469de785
kanstantsin-nekhniadovich <konstantyn.niechniedowicz@gmail.com>
2021-03-02 06:57:35 +0100

    fix: creating new project

--- src/ducks/tasks/selectors.ts
+++ src/ducks/tasks/selectors.ts
@@ -1,1 +1,3 @@
-export const getTasksForProject = (id: Id) => (store: Store) => store.tasks.tasks[id];
+import { Task } from '@typings';
+
+export const getTasksForProject = (id: Id) => (store: Store): Task[] | undefined => store.tasks.tasks[id];
