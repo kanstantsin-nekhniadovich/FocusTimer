a4458d7d61548401e8c05183d1a5851be8f0db83
kanstantsin-nekhniadovich <konstantyn.niechniedowicz@gmail.com>
2021-06-07 22:28:33 +0200

    chore: add test for LoginForm

--- src/store.ts
+++ src/store.ts
@@ -6,17 +6,17 @@ import { rootReducer, rootEpic } from './ducks';
 import { services } from './graphql/services';
 import { isDevEnv } from './utils/environment';
 
-const epicMiddleware = createEpicMiddleware<AppActions, AppActions, Store, Services>({
-  dependencies: {
-    ...services,
-  },
-});
-
-const enhancers = isDevEnv()
-  ? composeWithDevTools(applyMiddleware(epicMiddleware))
-  : applyMiddleware(epicMiddleware);
-
 export const configureStore = (initialState?: Partial<Store>) => {
+  const epicMiddleware = createEpicMiddleware<AppActions, AppActions, Store, Services>({
+    dependencies: {
+      ...services,
+    },
+  });
+
+  const enhancers = isDevEnv()
+    ? composeWithDevTools(applyMiddleware(epicMiddleware))
+    : applyMiddleware(epicMiddleware);
+
   const store = createStore(rootReducer, initialState, enhancers);
   epicMiddleware.run(rootEpic);
 
