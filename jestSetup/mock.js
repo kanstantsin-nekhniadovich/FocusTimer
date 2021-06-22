b8249d7ceec927e6abf904e3caeafb2f820382ca
kanstantsin-nekhniadovich <40122134+kanstantsin-nekhniadovich@users.noreply.github.com>
2021-06-22 21:37:18 +0200

    feat: add button to navigate to task screen (#62)

--- jestSetup/mock.js
+++ jestSetup/mock.js
@@ -1,3 +1,13 @@
+jest.mock('@react-navigation/native');
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+});
 jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
 jest.mock('expo-constants', () => ({
   manifest: {
--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -1,7 +1,10 @@
 import { Task } from '@typings';
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import Ripple from 'react-native-material-ripple';
 import { Text, View, Animated } from 'react-native';
+import { useNavigation, NavigationProp } from '@react-navigation/native';
+
 import { Colors, Typography } from '@styles';
 
 import { deleteTaskRequest } from '../../ducks';
@@ -12,12 +15,14 @@ import { Time, Tick } from '../icons';
 import { SwipeableRow } from './SwipeableRow';
 
 import { styles } from './styles';
+import { Routes } from '../../routes';
 
 interface Props {
   task: Task;
 }
 
 export const TaskItem: React.FC<Props> = ({ task }) => {
+  const navigation = useNavigation<NavigationProp<Screens, Routes.Project>>();
   const dispatch = useDispatch();
   const [isRemoveTaskModalOpened, setIsRemoveTaskModalOpened] = React.useState(false);
 
@@ -26,14 +31,20 @@ export const TaskItem: React.FC<Props> = ({ task }) => {
     close();
   };
 
-  const showModal = React.useCallback(() => {
+  const showModal = () => {
     setIsRemoveTaskModalOpened(true);
-  }, []);
+  };
 
   const removeTask = (close: () => void) => () => {
     dispatch(deleteTaskRequest(task.id));
     close();
   };
+
+  const onPress = () =>
+    navigation.navigate({
+      name: Routes.Task,
+      params: { projectId: task.projectId, id: task.id }
+    });
 
   const isNewTask = task.status === Status.TODO && task.workTime === task.remainingTime;
   const isCompletedTask = task.status === Status.COMPLETED && task.remainingTime === 0;
@@ -43,35 +54,37 @@ export const TaskItem: React.FC<Props> = ({ task }) => {
   return (
     <SwipeableRow onSwipeableOpenAction={showModal}>
       {({ close }) => (
-        <Animated.View style={styles.item}>
-          <View style={styles.titleHolder}>
-            {isCompletedTask
-              ? <Tick />
-              : <CircleProgressBar size={36} strokeWidth={3} progress={progress} progressColor={Colors.darkBlue} />
+        <Ripple onPress={onPress} rippleContainerBorderRadius={8}>
+          <Animated.View style={styles.item}>
+            <View style={styles.titleHolder}>
+              {isCompletedTask
+                ? <Tick />
+                : <CircleProgressBar size={36} strokeWidth={3} progress={progress} progressColor={Colors.darkBlue} />
+              }
+              <Text style={styles.title} numberOfLines={1}>{task.title}</Text>
+            </View>
+            {isNewTask
+              ? <Text style={styles.newTaskLabel}>New Task</Text>
+              : (!isCompletedTask &&
+                <View style={styles.timeHolder}>
+                  <Time />
+                  <Text style={styles.remainingTime}>{formatMillisecondsToMinutes(task.remainingTime)}</Text>
+                </View>)
             }
-            <Text style={styles.title} numberOfLines={1}>{task.title}</Text>
-          </View>
-          {isNewTask
-            ? <Text style={styles.newTaskLabel}>New Task</Text>
-            : (!isCompletedTask &&
-          <View style={styles.timeHolder}>
-            <Time />
-            <Text style={styles.remainingTime}>{formatMillisecondsToMinutes(task.remainingTime)}</Text>
-          </View>)
-          }
-          <ConfirmModal
-            title="Remove task"
-            isVisible={isRemoveTaskModalOpened}
-            onCancel={closeModal(close)}
-            onOk={removeTask(close)}
-            okText="Remove"
-            cancelText="Close"
-            closeOnClickOutside={false}
-            messageContent={<Text style={styles.confirmMessage}>
+            <ConfirmModal
+              title="Remove task"
+              isVisible={isRemoveTaskModalOpened}
+              onCancel={closeModal(close)}
+              onOk={removeTask(close)}
+              okText="Remove"
+              cancelText="Close"
+              closeOnClickOutside={false}
+              messageContent={<Text style={styles.confirmMessage}>
           Are you sure that you want to remove <Text style={Typography.subtitleLarge}>{task.title}</Text> task?
-            </Text>}
-          />
-        </Animated.View>
+              </Text>}
+            />
+          </Animated.View>
+        </Ripple>
       )}
     </SwipeableRow>
   );
